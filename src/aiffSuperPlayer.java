f35a5c7b5fff1adb3b5568782ac1409e4a659496
Érikson Augusto Melz <eamelz@furb.br>
2019-08-09 21:22:44 -0300

    Estrutura

--- src/aiffSuperPlayer.java
+++ src/aiffSuperPlayer.java
@@ -9,5 +9,34 @@
  * @author eamelz
  */
 public class aiffSuperPlayer {
+    //O construtor recebe o nome do arquivo a ser aberto e o abre,
+    //deixando-o pronto para execução.
     
+    public void play(){
+        //O método play() reproduz o arquivo aberto, a partir do ponto em que
+        //se encontra.
+    }
+    
+    public void pause(){
+        //O método pause() para a reprodução do arquivo, retornando a
+        //posição (segundos) em que se encontra.
+    }
+    
+    public void stop(){
+        //O método stop() para definitivamente a reprodução do arquivo. Não
+        //pode ser retomado por play() sem que antes seja definida uma
+        //posição (setCursor).
+    }
+    
+    public int setCursor(){
+        int i =0;
+        //O método setCursor(int) define uma nova posição (segundos) do arquivo. Pode estar em reprodução ou
+        //parado.
+        return i;
+    }
+    
+    public void release(){
+        //O método release() libera o arquivo da memória. Caso o arquivo esteja em reprodução, ele primeiro para
+        //(stop).
+    }
 }
