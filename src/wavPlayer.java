969c986dab581d691b341f0a7f5851176d871975
Érikson Augusto Melz <eamelz@furb.br>
2019-08-09 21:14:50 -0300

    Estrutura

--- src/wavPlayer.java
+++ src/wavPlayer.java
@@ -10,4 +10,22 @@
  */
 public class wavPlayer {
     
+    
+    public void setFile(String File){
+        //O método setFile() é utilizado para definir o nome do arquivo que será
+        //utilizado pelo objeto de reprodução de arquivos wma.
+    }
+    
+    public void setLocation(int local){
+        //O método setLocation() é utilizado para indicar a posição do arquivo
+        //(segundos) onde deve iniciar a reprodução. Para começar a partir do início
+        //deve-se fornecer como argumento o valor “0”.
+    }
+    
+    public int getLocation(){
+        int i = 0;
+        ///O método getLocation() retorna em que posição (segundos) se encontra a
+        //reprodução do arquivo.
+        return i;
+    }
 }
